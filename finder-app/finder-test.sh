81e97adb548af8691fed90645918674ea21489d5
Madeleine Monfort <mamo6538@colorado.edu>
2024-02-10 15:27:27 -0800

    fixing finder-test to work with PATH

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -5,11 +5,10 @@
 set -e
 set -u
 
-PATH_DIR=$(dirname $0)
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
-WRITEDIR=${PATH_DIR}/tmp/aeld-data
-username=$(cat ${PATH_DIR}/conf/username.txt)
+WRITEDIR=/tmp/aeld-data
+username=$(cat /etc/finder-app/conf/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -23,7 +22,7 @@ then
 else
 	NUMFILES=$1
 	WRITESTR=$2
-	WRITEDIR=${PATH_DIR}/tmp/aeld-data/$3
+	WRITEDIR=/tmp/aeld-data/$3
 fi
 
 MATCHSTR="The number of files are ${NUMFILES} and the number of matching lines are ${NUMFILES}"
@@ -33,7 +32,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat ${PATH_DIR}/conf/assignment.txt`
+assignment=`cat /etc/finder-app/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
@@ -55,15 +54,15 @@ fi
 
 for i in $( seq 1 $NUMFILES)
 do
-	${PATH_DIR}/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	${PATH}/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(${PATH_DIR}/finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(${PATH}/finder.sh "$WRITEDIR" "$WRITESTR")
 
 echo $OUTPUTSTRING > /tmp/assignment4-result.txt
 
 # remove temporary directories
-rm -rf ${PATH_DIR}/tmp/aeld-data
+rm -rf /tmp/aeld-data
 
 set +e
 echo ${OUTPUTSTRING} | grep "${MATCHSTR}"
