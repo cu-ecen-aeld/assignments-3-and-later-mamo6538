9ee4069ea257d81489fc09e89e9314a087497f51
Madeleine Monfort <mamo6538@colorado.edu>
2024-02-10 16:03:17 -0800

    first threading attempt

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,10 +10,29 @@
 
 void* threadfunc(void* thread_param)
 {
-
-    // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_data_p = (struct thread_data *) thread_param;
+    
+    //start with passing
+    thread_data_p->thread_complete_sucess = true;
+    
+    //wait for thread_data_p->wait_obtain_ms
+    usleep(thread_data_p->wait_obtain_ms); //usleep is the milisecond version of sleep
+    
+    //obtain thread_data_p->m
+    int result = pthread_mutex_lock(thread_data_p->m);
+    if(result != 0) thread_data_p->thread_complete_sucess = false;
+    
+    //wait for thread_data_p->wait_release_ms
+    usleep(thread_data_p->wait_release_ms);
+    
+    //release thread_data_p->m
+    result = pthread_mutex_unlock(thread_data_p->m);
+    
+    //set thread_data_p->thread_complete_sucess
+    if(result != 0) thread_data_p->thread_complete_sucess = false;
+    
     return thread_param;
 }
 
@@ -28,6 +47,23 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+     //allocate memory for thread_data
+     struct thread_data* td = (struct thread_data*)malloc(sizeof(struct thread_data));
+     //setup mutex and wait arguments
+     td->m = mutex;
+     td->wait_obtain_ms = wait_to_obtain_ms;
+     td->wait_release_ms = wait_to_release_ms;
+     
+     int rc = pthread_create(thread, NULL, threadfunc(), td);
+     
+     //check success
+     int result;
+     if(td->thread_complete_sucess) result = true;
+     else result = false;
+     
+     //malloc so need to free
+     free(td);
+     
+    return rc;
 }
 
--- examples/threading/threading.h
+++ examples/threading/threading.h
@@ -14,6 +14,9 @@ struct thread_data{
      * between the start_thread_obtaining_mutex function and
      * your thread implementation.
      */
+     int wait_obtain_ms;
+     int wait_release_ms;
+     pthread_mutex_t* m; //idk if we want this, but I think we do
 
     /**
      * Set to true if the thread completed with success, false
