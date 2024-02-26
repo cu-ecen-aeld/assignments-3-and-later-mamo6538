d658fa4baf4f37036e37e5df1a5e28aa4911a0d0
Madeleine Monfort <mamo6538@colorado.edu>
2024-02-25 21:57:31 -0800

    fixing a4

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -10,26 +10,33 @@
 
 void* threadfunc(void* thread_param)
 {
+    if(!thread_param) {
+    	return NULL;
+    }
+    
     // hint: use a cast like the one below to obtain thread arguments from your parameter
     //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
     struct thread_data* thread_data_p = (struct thread_data *) thread_param;
     
-    //start with passing
-    thread_data_p->thread_complete_success = true;
+    bool success = true;
     
     //wait for thread_data_p->wait_obtain_ms
-    usleep(thread_data_p->wait_obtain_ms); //usleep is the milisecond version of sleep
+    int rc = usleep(thread_data_p->wait_obtain_ms); //usleep is the milisecond version of sleep
+    if(rc != 0) success = false;
     
     //obtain thread_data_p->m
     int result = pthread_mutex_lock(thread_data_p->m);
-    if(result != 0) thread_data_p->thread_complete_success = false;
+    if(result != 0) success = false;
     
     //wait for thread_data_p->wait_release_ms
-    usleep(thread_data_p->wait_release_ms);
+    rc = usleep(thread_data_p->wait_release_ms);
+    if(rc != 0) success = false;
     
     //release thread_data_p->m
     result = pthread_mutex_unlock(thread_data_p->m);
-    if(result != 0) thread_data_p->thread_complete_success = false;
+    if(result != 0) success = false;
+    
+    thread_data_p->thread_complete_success = success;
     
     return thread_param;
 }
@@ -39,6 +46,9 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
 {
      //allocate memory for thread_data
      struct thread_data* td = (struct thread_data*)malloc(sizeof(struct thread_data));
+     if(!td) {
+     	return false;
+     }
      //setup mutex and wait arguments
      td->m = mutex;
      td->wait_obtain_ms = wait_to_obtain_ms;
@@ -46,8 +56,10 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      
      int rc = pthread_create(thread, NULL, &threadfunc, td);
      int result = true;
-     if(rc != 0) result = false;
-     else result = true;
+     if(rc != 0) {
+     	result = false;
+     	free(td);
+     }
      
      
     return result;
